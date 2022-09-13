794d814af44032a3780a8bbcd3482bf257e998d3
Simon Folkerts <simfolkerts@gmail.com>
2022-09-14 11:28:01 +1200

    first commit for front end

---
+++ front/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<script>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<style scoped></style>
