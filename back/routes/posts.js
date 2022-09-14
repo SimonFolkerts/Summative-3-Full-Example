2250f23dadebca65e01b4f9e5676f4ca5def9ae4
Simon Folkerts <simfolkerts@gmail.com>
2022-09-14 17:09:40 +1200

    add and cofigure multer to post endpoint to allow reciept and saving of files to the server. New Post document then reads saved file and transmits it to the database

--- back/routes/posts.js
+++ back/routes/posts.js
@@ -1,21 +1,47 @@
 const express = require("express");
 const router = express.Router();
 
+// multer setup
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+const upload = multer({
+  // setup storage
+  storage: multer.diskStorage({
+    // define the folder into which uploaded image goes
+    destination: function (req, file, cb) {
+      cb(null, "./uploads");
+    },
+    // define the filename to use
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+  // 5mb limit
+  limits: { fileSize: 5242880 },
+});
+
 // import the post model
 const Post = require("../models/Posts.js");
 
-router.post("/", (req, res) => {
-  console.log(req.body);
+// endpoint can now read and save a file with a fieldname of image
+router.post("/", upload.single("image"), (req, res) => {
   // create a new document from the post model using the request data
-  // exclude image for now, will add later
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
-    // TODO add images
+    image: {
+      // the image is read from the upload folder where multer put it
+      data: fs.readFileSync(path.join("./uploads/" + req.file.filename)),
+      // inform of what type of file it is
+      contentType: "image/png",
+    },
   });
 
   // save the new document, and once complete, send back the new document to the client
   post.save(() => {
+    fs.unlinkSync(path.join("./uploads/" + req.file.filename));
     res.json(post);
   });
 });
