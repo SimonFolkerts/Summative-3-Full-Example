31a7ed6fb0d54719d797c2a696cd87eba86f0ef4
Simon Folkerts <simfolkerts@gmail.com>
2022-09-21 13:16:20 +1200

    accidentally forgot to add sameSite and secure to the token for logout

--- back/routes/authentication.js
+++ back/routes/authentication.js
@@ -59,7 +59,7 @@ router.post("/login", async (req, res) => {
 
 // for logout what should occur is the removal of the jwt cookie. This is not directly possible, but what we can do is replace it with a new jwt cookie with a very short expiry time
 router.get("/logout", (req, res) => {
-  res.cookie("jwt", "", { maxAge: 1 });
+  res.cookie("jwt", "", { maxAge: 1, sameSite: "none", secure: true });
   res.json({ message: "hello" });
 });
 
